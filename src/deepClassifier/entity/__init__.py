11669ec9e07019f90649dbf436c61f279c9f0ff0
BalaM14 <balamurugan141196@gmail.com>
2024-06-22 16:56:16 +0530

    Stage 04 Completed

--- src/deepClassifier/entity/__init__.py
+++ src/deepClassifier/entity/__init__.py
@@ -2,5 +2,6 @@ from deepClassifier.entity.config_entity import (
     DataIngestionConfig, 
     PrepareBaseModelConfig, 
     PrepareCallBackConfig, 
-    TrainingConfig
+    TrainingConfig,
+    EvaluationConfig
 )
