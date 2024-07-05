d815860cd1895d69cd32c27f18125aafe68ceec0
BalaM14 <balamurugan141196@gmail.com>
2024-07-05 14:28:29 +0530

    updated app.py

--- app.py
+++ app.py
@@ -23,6 +23,8 @@ if uploaded_file is not None:
     argmax_index = np.argmax(result, axis=1) # [0, 0]
     st.header('PREDICTED OUTPUT', divider='rainbow')
     if argmax_index[0] == 0:
-        st.image(image, caption="CAT")
+        st.image(image)
+        st.header(':blue[CAT]')
     else:
-        st.image(image, caption='DOG',)
+        st.image(image)
+        st.header('DOG')
